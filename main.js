1d7b9fac58124325670dcbb168fb1854bcf7ea39
Mike <MikeAMarchlewski@users.noreply.github.com>
2018-08-22 18:53:05 -0500

    End of inital list before refactoring

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-var todos = [];
+var todos = ["Run a 5k"];
 
 var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
     //handle input
     if(input === "list") {
-        console.log(todos);
-    } else if(input=== "new") {
+        console.log("**********")
+        todos.forEach(function(todo, i){
+        console.log(i+ ": " + todo);
+        });
+        console.log("**********")
+    }   else if(input=== "new") {
         //ask for new todo
         var newTodo = prompt("Enter new todo");
         //add to todos array
         todos.push(newTodo);
-        //At this point in the code allong there is no way to keep this repeating until the user quits. It is one and done so need a loop. This note is pre while loop added.
-}
+        //At this point in the code along there is no way to keep this repeating until the user quits. It is one and done so need a loop. This note is pre while loop added.
+        console.log("Added Todo");
+    }   else if(input === "delete"){
+        //ask for index of Todo to be deleted
+        var index = prompt("Enter index of todo to delete")
+        //delete the todo
+        //splice()
+        todos.splict(index,1);
+        console.log("Deleted Todo");
+    }  
     //ask again for input.
     var input = prompt("What would else you like to do?");
 
