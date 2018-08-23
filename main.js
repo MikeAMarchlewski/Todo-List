46a69d263e42281861e4ed418e6533658e0bfad2
Mike <MikeAMarchlewski@users.noreply.github.com>
2018-08-22 19:02:06 -0500

    End of project

--- main.js
+++ main.js
@@ -5,28 +5,40 @@ var input = prompt("What would you like to do?");
 while(input !== "quit"){
     //handle input
     if(input === "list") {
-        console.log("**********")
-        todos.forEach(function(todo, i){
-        console.log(i+ ": " + todo);
-        });
-        console.log("**********")
+        listTodos();
     }   else if(input=== "new") {
         //ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add to todos array
-        todos.push(newTodo);
-        //At this point in the code along there is no way to keep this repeating until the user quits. It is one and done so need a loop. This note is pre while loop added.
-        console.log("Added Todo");
+        addTodo();
     }   else if(input === "delete"){
         //ask for index of Todo to be deleted
-        var index = prompt("Enter index of todo to delete")
-        //delete the todo
-        //splice()
-        todos.splict(index,1);
-        console.log("Deleted Todo");
+        deleteTodo();
     }  
     //ask again for input.
     var input = prompt("What would else you like to do?");
 
 }
 console.log("Ok, You have quit the App.");
+
+function listTodos(){
+    console.log("**********")
+    todos.forEach(function(todo, i){
+    console.log(i+ ": " + todo);
+    });
+    console.log("**********")
+};
+
+function addTodo(){
+    var newTodo = prompt("Enter new todo");
+        //add to todos array
+        todos.push(newTodo);
+        
+        console.log("Added Todo");
+};
+
+function deleteTodo(){
+    var index = prompt("Enter index of todo to delete")
+        //delete the todo
+        //splice() deletes a specific item from an array
+        todos.splice(index,1);
+        console.log("Deleted Todo");
+};
